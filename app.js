85e769b4c8496d7b57de10ef68ca2ab72dcd034f
alexandr <alexandr87m@gmail.com>
2023-01-16 10:54:21 +0500

    Add keyboard events

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ downBtn.addEventListener('click', () => {
     changeSlide('down');
 });
 
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up');
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down');
+    }
+})
+
 function changeSlide (direction) {
     if (direction === 'up') {
         activeSlideIndex++;
